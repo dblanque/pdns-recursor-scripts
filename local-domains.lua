8a34500136ed8b13fd05254a78f20a42d8f5bbc8
dblanque <dylan.blanque@gmail.com>
2024-01-15 17:46:29 -0300

    Fix function indexation

--- local-domains.lua
+++ local-domains.lua
@@ -40,7 +40,7 @@ if g.options.use_local_forwarder then
 
 	pdnslog("Loading preresolve_lo into pre-resolve functions.", pdns.loglevels.Notice)
 	table.insert(g.preresolve_index, 'preresolve_lo')
-	g.preresolve_functions[table_len(g.preresolve_index)] = preresolve_lo
+	g.preresolve_functions['preresolve_lo'] = preresolve_lo
 else
 	pdnslog("Local Domain Forwarder not enabled. Set overrides in file overrides.lua", pdns.loglevels.Notice)
 end

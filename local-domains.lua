4a0ed3934ef705ba9599d8320be902f27bb31ccf
dblanque <dylan.blanque@gmail.com>
2024-01-16 12:13:22 -0300

    Fix return true loc

--- local-domains.lua
+++ local-domains.lua
@@ -40,15 +40,14 @@ function preresolve_lo(dq)
 
 		if dq.qtype == pdns.A or dq.qtype == pdns.ANY then
 			dq:addAnswer(pdns.A, g.options.private_zones_resolver_v4)
+			return true
 		end
 
 		if dq.qtype == pdns.AAAA or dq.qtype == pdns.ANY then
 			dq:addAnswer(pdns.AAAA, g.options.private_zones_resolver_v6)
+			return true
 		end
-		
-		return true
-	end
-	
+
 	-- default, do not rewrite this response
 	return false
 end

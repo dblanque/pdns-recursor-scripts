b7be8cfcab1e7fb7f81349ef09b7200fc39a4792
dblanque <dylan.blanque@gmail.com>
2024-01-16 12:26:16 -0300

    Change return true to eof

--- local-domains.lua
+++ local-domains.lua
@@ -41,18 +41,17 @@ function preresolve_lo(dq)
 		end
 	end
 
+	local modified = false
 	if dq.qtype == pdns.A or dq.qtype == pdns.ANY then
 		dq:addAnswer(pdns.A, g.options.private_zones_resolver_v4)
-		return true
 	end
 
 	if dq.qtype == pdns.AAAA or dq.qtype == pdns.ANY then
 		dq:addAnswer(pdns.AAAA, g.options.private_zones_resolver_v6)
-		return true
 	end
 
 	-- default, do not rewrite this response
-	return false
+	return true
 end
 
 -- Add preresolve function to table

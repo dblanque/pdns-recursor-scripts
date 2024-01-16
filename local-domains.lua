101cc59fa37647abed473ba9ac44f04c72b1609c
dblanque <dylan.blanque@gmail.com>
2024-01-16 12:26:28 -0300

    Remove apparently function braking param

--- local-domains.lua
+++ local-domains.lua
@@ -35,7 +35,7 @@ function preresolve_lo(dq)
 				"dns."..parent
 			}
 			for i, ns in ipairs(new_ns) do
-				dq:addAnswer(pdns.NS, ns, 300)
+				dq:addAnswer(pdns.NS, ns)
 			end
 			return true
 		end

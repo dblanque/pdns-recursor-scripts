2eaf7ed7567a5d35d8bd0dba57ccfe51285aeb02
dblanque <dylan.blanque@gmail.com>
2024-01-16 12:03:27 -0300

    Use toString on local zone

--- local-domains.lua
+++ local-domains.lua
@@ -18,8 +18,9 @@ function preresolve_lo(dq)
 	-- check blocklist
 	if local_domain_overrides:check(dq.qname) then
 		if dq.qtype == pdns.NS and g.options.private_zones_ns_override then
-			local ns_check = local_domain_overrides:toString(dq.qname)
+			local ns_check = local_domain_overrides:check(dq.qname)
 			if ns_check then
+				local zone = local_domain_overrides:toString()
 				local new_ns = {
 					"ns1."..zone,
 					"ns2."..zone,

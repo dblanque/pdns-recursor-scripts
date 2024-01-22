a9fa7f2527eb32b0af217e1cd125cc1c0634ab63
dblanque <dylan.blanque@gmail.com>
2024-01-22 15:13:58 -0300

    Change back to require

--- overrides-handler.lua
+++ overrides-handler.lua
@@ -7,13 +7,12 @@ local function get_lua_modules_in_conf(search_dir)
 	local files = {}
 	for dir in io.popen("ls -pa " .. search_dir .. " | grep -v /|grep -E \"*(.lua)\""):lines() 
 	do
-		-- table.insert(files, string.gsub(dir, '%.lua', '')[0])
-		table.insert(files, g.pdns_scripts_path .. '/conf.d/' .. dir)
+		table.insert(files, string.gsub(dir, '%.lua', '')[0])
 	end
 	return files
 end
 
 for index, lua_file in ipairs(get_lua_modules_in_conf(g.pdns_scripts_path .. '/conf.d')) do
 	pdnslog("Loading config file: " .. lua_file, pdns.loglevels.Notice)
-	dofile(lua_file)
+	require(lua_file)
 end

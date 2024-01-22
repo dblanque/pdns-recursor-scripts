55f326afbbf2089eb7cab90aa18a898a2bfa7e3d
dblanque <dylan.blanque@gmail.com>
2024-01-22 15:19:34 -0300

    Change to dofile (overrides-handler)

--- overrides-handler.lua
+++ overrides-handler.lua
@@ -1,21 +1,26 @@
 -- Do not modify this file, it's maintained by the repo.
 -- Add your overrides into the conf.d directory instead 
 -- (you may copy and options.lua there and modify what you want)
-package.path = package.path .. ";"..g.pdns_scripts_path.."/conf.d/?.lua"
+local conf_d_path = g.pdns_scripts_path .. '/conf.d'
+package.path = package.path .. ";".. conf_d_path .. "/?.lua"
 local options_overrides = {}
 
-local function get_lua_modules_in_conf(search_dir)
+local function get_lua_modules_in_conf(search_dir, fullpath)
 	local files = {}
 	for dir in io.popen("ls -pa " .. search_dir .. " | grep -v /|grep -E \"*(.lua)\""):lines() 
 	do
-		table.insert(files, string.gsub(dir, '%.lua', '')[0])
+		if fullpath then
+			table.insert(files, conf_d_path .. "/" .. dir)
+		else
+			table.insert(files, string.gsub(dir, '%.lua', '')[0])
+		end
 	end
 	return files
 end
 
-for index, lua_file in ipairs(get_lua_modules_in_conf(g.pdns_scripts_path .. '/conf.d')) do
+for index, lua_file in ipairs(get_lua_modules_in_conf(conf_d_path, true)) do
 	pdnslog("Loading config file: " .. lua_file, pdns.loglevels.Notice)
-	require(lua_file)
+	dofile(lua_file)
 end
 
 return options_overrides

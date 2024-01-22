dc08f56b2b440829bd34f8519a7fbc7630b3d7d3
dblanque <dylan.blanque@gmail.com>
2024-01-22 15:35:41 -0300

    Attempt to fix overrides import

--- overrides-handler.lua
+++ overrides-handler.lua
@@ -3,7 +3,6 @@
 -- (you may copy and options.lua there and modify what you want)
 local conf_d_path = g.pdns_scripts_path .. '/conf.d'
 local options_overrides = {}
-package.path = package.path .. ";".. conf_d_path .. "/?.lua"
 
 local function get_lua_modules_in_conf(search_dir, fullpath)
 	local files = {}
@@ -18,12 +17,12 @@ local function get_lua_modules_in_conf(search_dir, fullpath)
 	return files
 end
 
-local conf_files = get_lua_modules_in_conf(conf_d_path, false)
+local conf_files = get_lua_modules_in_conf(conf_d_path, true)
 local conf_files_l = table_len(conf_files)
 
 for index, lua_file in ipairs(conf_files) do
 	pdnslog("Loading config file: " .. lua_file, pdns.loglevels.Notice)
-	require(lua_file)
+	dofile(lua_file)
 
 	if index >= conf_files_l then
 		pdnslog("Table Length: "..table_len(options_overrides), pdns.loglevels.Notice)

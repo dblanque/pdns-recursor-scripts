f87201f3e4f22450aa95d18581c03135763f625c
dblanque <dylan.blanque@gmail.com>
2024-01-22 18:27:44 -0300

    Add table printing to show-settings

--- show-settings.lua
+++ show-settings.lua
@@ -19,7 +19,14 @@ if not g.options_overrides then
 	pdnslog("Could not import overrides correctly (or there are none).")
 else
 	for k, v in pairs(g.options_overrides) do
-		pdnslog("Loaded Option (".. tostring(k) .."): "..tostring(v))
+		if type(v) ~= 'table' then
+			pdnslog("Loaded Option (".. tostring(k) .."): "..tostring(v))
+		else
+			pdnslog("Loaded Option (".. tostring(k) .."): "..tostring(v))
+			for index, value in pairs(v) do
+				pdnslog(index, value)
+			end
+		end
 		g.options[k] = v
 	end
 	pdnslog("Loaded ".. f.table_len(g.options_overrides) .." overrides")

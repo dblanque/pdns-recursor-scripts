bfb178ea4581a03de08c28cf5568de7486956dc1
dblanque <dylan.blanque@gmail.com>
2024-01-22 15:33:31 -0300

    Fix wrong var usage

--- overrides-handler.lua
+++ overrides-handler.lua
@@ -18,12 +18,12 @@ local function get_lua_modules_in_conf(search_dir, fullpath)
 	return files
 end
 
-local conf_files = get_lua_modules_in_conf(conf_d_path, true)
+local conf_files = get_lua_modules_in_conf(conf_d_path, false)
 local conf_files_l = table_len(conf_files)
 
 for index, lua_file in ipairs(conf_files) do
 	pdnslog("Loading config file: " .. lua_file, pdns.loglevels.Notice)
-	dofile(filename)
+	require(lua_file)
 
 	if index >= conf_files_l then
 		pdnslog("Table Length: "..table_len(options_overrides), pdns.loglevels.Notice)

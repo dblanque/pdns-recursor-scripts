5dcfa0a4767bb9efde512d1fe348929d629224e1
dblanque <dylan.blanque@gmail.com>
2024-01-22 16:42:39 -0300

    Changes to example cfg

--- conf.d/example.lua
+++ conf.d/example.lua
@@ -1,5 +1,6 @@
 -- Beware, this file gets directly included into the hooks.lua file
--- Recommended name: overrides.lua
+-- You can load multiple config files, repeated options will be replaced by the last file.
+-- Recommended names: overrides.lua || settings.lua || conf_dnsbl.lua || conf_local.lua
 return {
 	-- Put your options in here
 }

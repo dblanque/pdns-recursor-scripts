a486b2922ca571903d87dc058f4020a425fdb2bc
dblanque <dylan.blanque@gmail.com>
2024-01-22 17:12:40 -0300

    Add stub pdnslog function to show-settings

--- show-settings.lua
+++ show-settings.lua
@@ -4,6 +4,11 @@ function script_path()
 	return str:match("(.*/)") or "."
 end
 
+local function pdnslog(msg, loglevel)
+	-- Stub function
+	print(msg)
+end
+
 g = {}
 f = require('functions')
 g.pdns_scripts_path = script_path()
@@ -11,11 +16,11 @@ g.options = require('defaults')
 g.options_overrides = require('overrides-handler')
 package.path = package.path .. ";"..g.pdns_scripts_path.."/?.lua"
 if not g.options_overrides then
-	print("Could not import overrides correctly (or there are none).")
+	pdnslog("Could not import overrides correctly (or there are none).")
 else
 	for k, v in pairs(g.options_overrides) do
-		print("Loaded Option (".. tostring(k) .."): "..tostring(v))
+		pdnslog("Loaded Option (".. tostring(k) .."): "..tostring(v))
 		g.options[k] = v
 	end
-	print("Loaded ".. f.table_len(g.options_overrides) .." overrides")
+	pdnslog("Loaded ".. f.table_len(g.options_overrides) .." overrides")
 end

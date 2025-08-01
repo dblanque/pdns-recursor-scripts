fedb00a212b65e7307d69f2a4737df6334ca1585
Dylan Blanque <dylan.blanque@gmail.com>
2025-08-01 14:32:44 -0300

    Improve code formatting.

--- hooks.lua
+++ hooks.lua
@@ -11,25 +11,49 @@ f = require('functions')
 g.options = require('defaults')
 g.options_overrides = require('overrides-handler')
 if not g.options_overrides then
-	pdnslog("Could not import overrides correctly (or there are none).", pdns.loglevels.Error)
+	pdnslog(
+		"Could not import overrides correctly (or there are none).",
+		pdns.loglevels.Error
+	)
 else
 	for k, v in pairs(g.options_overrides) do
-		pdnslog("Loaded Option (".. tostring(k) .."): "..tostring(v), pdns.loglevels.Debug)
+		pdnslog(
+			"Loaded Option (".. tostring(k) .."): "..tostring(v),
+			pdns.loglevels.Debug
+		)
 		g.options[k] = v
 	end
-	pdnslog("Loaded ".. f.table_len(g.options_overrides) .." overrides", pdns.loglevels.Notice)
+	pdnslog(
+		string.format(
+			"Loaded %d overrides",
+			f.table_len(g.options_overrides)
+		)
+		pdns.loglevels.Notice
+	)
 end
 
 require("local-domains")
 require("malware-filter")
 
-pdnslog("preresolve function table contains " .. f.table_len(g.preresolve_functions) .. " entries.", pdns.loglevels.Notice)
-pdnslog("postresolve function table contains " .. f.table_len(g.postresolve_functions) .. " entries.", pdns.loglevels.Notice)
+pdnslog(
+	string.format(
+		"preresolve function table contains %d entries.",
+		f.table_len(g.preresolve_functions)
+	),
+	pdns.loglevels.Notice
+)
+pdnslog(
+	string.format(
+		"postresolve function table contains %d entries.",
+		f.table_len(g.postresolve_functions)
+	),
+	pdns.loglevels.Notice
+)
 for i, k in ipairs(g.preresolve_index) do
-	pdnslog(k.." preresolve function loaded.", pdns.loglevels.Debug)
+	pdnslog(k.." preresolve function added.", pdns.loglevels.Debug)
 end
 for i, k in ipairs(g.postresolve_index) do
-	pdnslog(k.." postresolve function loaded.", pdns.loglevels.Debug)
+	pdnslog(k.." postresolve function added.", pdns.loglevels.Debug)
 end
 
 function preresolve(dq)

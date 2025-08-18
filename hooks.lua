929e8ce994df956c84d976e919b556e37d2c038d
Dylan Blanque <dylan.blanque@gmail.com>
2025-08-18 14:30:22 -0300

    Change dq.data initialization to only happen if not nil.

--- hooks.lua
+++ hooks.lua
@@ -67,7 +67,11 @@ for i, k in ipairs(g.postresolve_index) do
 end
 
 function preresolve(dq)
-	dq.data = {}
+	-- Initialize persistent data table
+	if not dq.data then
+		dq.data = {}
+	end
+
 	for index, f_name in ipairs(g.preresolve_index) do
 		local pre_r_f = g.preresolve_functions[f_name]
 		if not pre_r_f then
